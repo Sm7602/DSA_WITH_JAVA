f1e12ad9756c37210a8849287d8164709dc7114d
Souvik7602 <sm2496444@gmail.com>
2025-10-21 02:00:06 +0530

    update

--- src/_03_LinkedList/_02_implementation.java
+++ src/_03_LinkedList/_02_implementation.java
@@ -11,6 +11,7 @@ public class _02_implementation {
 	public static class LinkedList{
 		Node head;
 		Node tail;
+		//add data in likedlist
 		void add(int data) {
 			Node newdata=new Node(data);
 			if(head==null){
@@ -21,6 +22,7 @@ public class _02_implementation {
 			}
 			tail=newdata;
 		}
+		//add data in head
 		void addFrist(int data) {
 			Node newdata=new Node(data);
 			if(head==null){
@@ -31,6 +33,7 @@ public class _02_implementation {
 			}
 			 head=newdata;
 		}
+		//add data by index
 		void addAt(int index,int data) {
 			Node newdata=new Node(data);
 			Node temp=head;
@@ -40,9 +43,11 @@ public class _02_implementation {
 			newdata.next=temp.next;
 			temp.next=newdata;
 		}
+		//add data in tail
 		void addLast(int data) {
 			add(data);
 		}
+		// size of linkedlist
 		int length() {
 			Node temp=head;
 			int c=0;
@@ -52,9 +57,11 @@ public class _02_implementation {
 			}
 			return c;
 		}
+		//remove first data
 		void removeFrist() {
 			head=head.next;
 		}
+		//remove last data
 		void removeLast() {
 			Node temp=head;
 			while(temp.next.next != null) {
@@ -62,14 +69,17 @@ public class _02_implementation {
 			}
 			temp.next=null;
 		}
+		//remove all data
 		void removeAll() {
 			head=null;
 			tail=null;
 		}
+		//remove  node by node val
 		void removeNode(Node node ) {
 			node.data=node.next.data;
 			node.next=node.next.next;
 		}
+		//remove index data
 		void removeAt(int index) {
 			Node temp=head;
 			for(int i=1;i<=index-1;i++) {
@@ -78,26 +88,44 @@ public class _02_implementation {
 			temp.data=temp.next.data;
 			temp.next=temp.next.next;
 		}
+		//remove data by val
+		void remove(int data) {
+			Node temp=head;
+			while(temp!=null) {
+				if(temp.data==data) {
+					temp.data=temp.next.data;
+					temp.next=temp.next.next;
+				}else {
+				temp=temp.next;
+				}
+			}
+		}
+		//revarce linkedlist
 		void revarce(Node head) {
 			if(head == null) return;
 			revarce(head.next);
 			System.out.print(head.data+" ");
 		}
+		//sorting of linked list
 		void sort() {
-			
+			Node h=head;
+			for(int j=1;j<length();j++) {
 				for(int i=1;i<length();i++) {
-					if(head.data>head.next.data) {
-						int temp=head.next.data;
-						head.next.data=head.data;
-						head.data=temp;
+					if(h.data>h.next.data) {
+						int temp=h.next.data;
+						h.next.data=h.data;
+						h.data=temp;
 					}
 					else {
-						head.data=head.data;
+						h.data=h.data;
 					}
-					head=head.next;
+					h=h.next;
 				}
+				h=head;
+			}
 			
 		}
+		//find data by index
 		boolean search(int index) {
 			Node temp=head;
 			boolean b=false;
@@ -109,9 +137,22 @@ public class _02_implementation {
 				temp=temp.next;
 				c++;
 			}
-			
 			return b;
 		}
+		//find data by val
+		boolean searchbyVal(int data) {
+			Node temp=head;
+			boolean b=false;
+			while(temp!=null) {
+				if(data==temp.data) {
+					b=true;
+				}
+				temp=temp.next;
+			}
+			return b;
+		}
+	
+		//display
 		void display() {
 			Node temp=head;
 			while(temp !=null) {
@@ -130,7 +171,6 @@ public class _02_implementation {
 		ll.addLast(1);
 		ll.addFrist(9);
 		ll.addFrist(10);
-		ll.display();
 		ll.addAt(3,5);
 		ll.display();
 //		ll.sort();
@@ -143,11 +183,17 @@ public class _02_implementation {
 //		ll.removeLast();
 //		ll.display();
 //		ll.removeAll();
-//		 ll.removeNode(ll.head.next.next.next);
-//		 ll.display();
+//		ll.removeNode(ll.head.next.next.next);
+//		ll.remove(7);
+//		ll.display();
 		System.out.print(ll.length());
+		System.out.println();
 //		ll.revarce(ll.head);
-		System.out.print(ll.search(8));
+//		System.out.print(ll.search(8));
+		ll.sort();
+		ll.display();
+		System.out.print(ll.searchbyVal(10));
+		
 	
 	}
 
